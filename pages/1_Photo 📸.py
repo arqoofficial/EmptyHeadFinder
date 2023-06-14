976a9f8d67aeb67486dd6a18c37d02374e967b6c
arqoofficial <argolubev97@yandex.ru>
2023-06-14 13:07:29 +0300

    fix: Update st pages according to new core

--- pages/1_Photo 📸.py
+++ pages/1_Photo 📸.py
@@ -1,47 +1,18 @@
-import os
 import streamlit as st
+
 import streamlit_core as stc
-import media_processing as mp
+
 
 st.set_page_config(
     layout="centered",
     page_title="Photo",
     page_icon="📸"
 )
-
 st.write("# Photo 📸")
 st.write("___")
 
-model = stc.set_model()
-supported_formats = ["png", "jpg", "jpeg"]
-supported_formats_str = ", ".join(str(format) for format in supported_formats)
-help_image = f"Supported image formats: {supported_formats_str}"
-images = st.file_uploader(
-    "Upload your photo",
-    accept_multiple_files=True,
-    type=supported_formats,
-    help=help_image
-)
-
-tmp_folder_path = os.path.relpath("./images/tmp/")
-if os.path.exists(tmp_folder_path):
-    pass
-else:
-    os.mkdir(tmp_folder_path)
-
-analyze_button = st.button("Analyze! 🎲")
-if analyze_button:
-    mp.clear_tmp(tmp_folder_path)
-    if images:
-        for image in images:
-            with st.spinner(text="In progress..."):
-                image_path = os.path.join(tmp_folder_path, image.name)
-                with open(image_path, "wb") as temp_file:
-                    temp_file.write(image.read())
-                stc.analyze_image(
-                    model=model,
-                    image=image_path
-                )
-    else:
-        st.warning("Upload some images!")
-mp.clear_tmp(tmp_folder_path)
+model = stc.load_model_st()
+model = stc.set_model_st(model)
+images = stc.upload_media_st()
+stc.create_tmp_folder_st()
+stc.analyze_media_st(images, model)
--- pages/2_Video 📹.py
+++ pages/2_Video 📹.py
@@ -1,8 +1,6 @@
-import os
 import streamlit as st
+
 import streamlit_core as stc
-import media_processing as mp
-import zipfile
 
 
 st.set_page_config(
@@ -10,81 +8,11 @@ st.set_page_config(
     page_title="Video",
     page_icon="📹"
 )
-
-
 st.write("# Video 📹")
 st.write("___")
 
-
-model = stc.set_model()
-
-process_speed = stc.set_process_speed()
-
-supported_formats = ["mp4", "avi"]
-supported_formats_str = ", ".join(str(format) for format in supported_formats)
-help_video = f"Supported video formats: {supported_formats_str}"
-
-video_files = st.file_uploader(
-    "Upload your video file(s)",
-    accept_multiple_files=True,
-    type=supported_formats,
-    help=help_video
-)
-
-temp_folder_path = os.path.relpath("./videos/tmp")
-if os.path.exists(temp_folder_path):
-    pass
-else:
-    os.mkdir(temp_folder_path)
-
-
-start_button = st.button("Analyze 🎲")
-outfiles_list = []
-
-if start_button:
-    outfiles_list = []
-    mp.clear_tmp(temp_folder_path)
-
-    if video_files:
-        with st.spinner(text="In progress..."):
-            for video_file in video_files:
-                video_file_path = os.path.join(
-                    temp_folder_path, video_file.name
-                )
-                with open(video_file_path, "wb") as temp_file:
-                    temp_file.write(video_file.read())
-
-                out_file = mp.video_processing(
-                    model,
-                    video_file_path,
-                    out_path=temp_folder_path,
-                    process_speed=process_speed
-                )
-
-                outfiles_list.append(out_file)
-
-                # Compile an archive of video reports
-                zip_file_path = os.path.join(
-                    temp_folder_path,
-                    "out_videos.zip"
-                )
-                with zipfile.ZipFile(
-                    zip_file_path,
-                    mode="a",
-                    compression=zipfile.ZIP_DEFLATED
-                ) as zip_file:
-                    zip_file.write(out_file)
-    else:
-        st.error("Choose file(s)")
-
-
-if outfiles_list:
-    st.success("The video report is ready", icon="✅")
-    with open(zip_file_path, "rb") as zip_file:
-        download_button = st.download_button(
-            label="Download archive",
-            data=zip_file,
-            file_name=zip_file_path
-        )
-    mp.clear_tmp(temp_folder_path)
-    outfiles_list = []
+model = stc.load_model_st()
+model = stc.set_model_st(model)
+videos = stc.upload_media_st(is_photo=False)
+stc.create_tmp_folder_st(is_photo=False)
+stc.analyze_media_st(videos, model, is_photo=False)
