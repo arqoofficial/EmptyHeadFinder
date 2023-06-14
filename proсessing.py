0a8b0149c68a5f141a1e1c193dfef6f3edd19212
arqoofficial <argolubev97@yandex.ru>
2023-06-14 13:04:26 +0300

    fix: Rename and update processing.py

--- proсessing.py
+++ proсessing.py
@@ -1,4 +1,6 @@
+"""Processing functions for tkinter app (single.py) and streamlit_core.py"""
 import os
+from typing import Tuple
 
 import cv2
 from ultralyticsplus import YOLO
@@ -9,11 +11,10 @@ def load_model(
     conf: float = 0.3,
     iou: float = 0.45,
     agnostic_nms: bool = False,
-    max_det: int = 1000
+    max_det: int = 1000,
 ) -> YOLO:
-    """Loads a YOLOv8 model for the further photo or video processing"""
-    
-    # Check model_size input
+    """Loads a YOLOv8 model for the further photo or video processing.
+    You can also specify model parameters if necessary."""
     model_sizes_list = ["n", "s", "m"]
     if model_size in model_sizes_list:
         model_name = f"keremberke/yolov8{model_size}-hard-hat-detection"
@@ -21,49 +22,48 @@ def load_model(
         raise ValueError(
             f"Input correct model size! Choose from {model_sizes_list}"
         )
-    
-    # Load model
-    model = YOLO(model_name)
 
-    # Check model's parameters
-    if 0 < conf <= 1:
+    if 0 <= conf <= 1:
         pass
     else:
         raise ValueError(
-            f"Input correct conf value (should be in range (0, 1])!"
+            "Input correct conf value (should be in range [0, 1])!"
         )
-        
-    if 0 < iou <= 1:
+
+    if 0 <= iou <= 1:
         pass
     else:
         raise ValueError(
-            f"Input correct iou value (should be in range (0, 1])!"
+            "Input correct iou value (should be in range [0, 1])!"
         )
 
     if isinstance(agnostic_nms, bool):
         pass
     else:
         raise ValueError(
-            f"Input correct agnostic_nms value (should be bool)!"
+            "Input correct agnostic_nms value (should be bool)!"
         )
-        
-    if isinstance(max_det, int) and 0 < max_det <= 1000:
+
+    if isinstance(max_det, int) and 0 <= max_det <= 1000:
         pass
     else:
         raise ValueError(
-            f"Input correct max_det (should be int and in range (0, 1000])!"
+            "Input correct max_det (should be int in range [0, 1000])!"
         )
-    
-    # Change model's parameters
-    model.overrides['conf'] = conf
-    model.overrides['iou'] = iou
-    model.overrides['agnostic_nms'] = agnostic_nms
-    model.overrides['max_det'] = max_det
+
+    model = YOLO(model_name)
+
+    model.overrides["conf"] = conf
+    model.overrides["iou"] = iou
+    model.overrides["agnostic_nms"] = agnostic_nms
+    model.overrides["max_det"] = max_det
 
     return model
 
 
-def get_video_stats(vid_capture: cv2.VideoCapture) -> tuple:
+def get_video_stats(
+    vid_capture: cv2.VideoCapture
+) -> Tuple[tuple, int, float]:
     """Gets the statistic of video"""
     frame_width = int(vid_capture.get(3))
     frame_height = int(vid_capture.get(4))
@@ -76,26 +76,45 @@ def get_video_stats(vid_capture: cv2.VideoCapture) -> tuple:
     return frame_size, fps, video_time
 
 
-def create_videoreport(out_path: str,
-                       filename: str,
-                       video_param: tuple) -> cv2.VideoWriter:
+def create_videoreport(
+    out_path: str,
+    filename: str,
+    video_param: tuple
+) -> cv2.VideoWriter:
     """Creates video report file"""
     frame_size = video_param[0]
     fps = video_param[1]
 
-    report_videofile = os.path.join(out_path, f"out_{filename}")
-    out_video = cv2.VideoWriter(report_videofile,
-                                cv2.VideoWriter_fourcc(*"XVID"),
-                                fps,
-                                frame_size)
+    out_video_path = os.path.join(out_path, f"out_{filename}")
+    out_video = cv2.VideoWriter(
+        out_video_path,
+        cv2.VideoWriter_fourcc(*"XVID"),
+        fps,
+        frame_size
+    )
+
     return out_video
 
 
-def video_processing(vid_capture: cv2.VideoCapture,
-                     video_report: cv2.VideoWriter,
-                     model: YOLO,
-                     process_speed: int = 1,
-                     show_violation_frames: bool = False) -> None:
+def detect(image: any, model: YOLO) -> bool:
+    """Using YOLOv8 model, detects people without a hardhat in the photo"""
+    results = model.predict(image)
+
+    # Iterations over tensors in order to locate the necessary objects
+    for box in results[0].boxes:
+        if int(box.cls) == 1:
+            return True
+
+    return False
+
+
+def video_processing(
+    vid_capture: cv2.VideoCapture,
+    video_report: cv2.VideoWriter,
+    model: YOLO,
+    process_speed: int = 1,
+    show_violation_frames: bool = False,
+) -> None:
     """Main function of video processing"""
     frame_counter = 0
     record_frame_counter = 0
@@ -127,16 +146,3 @@ def video_processing(vid_capture: cv2.VideoCapture,
 
     vid_capture.release()
     cv2.destroyAllWindows()
-
-
-def detect(image: any, model: YOLO) -> bool:
-    """Using YOLOv8 model, detects people without a hard hat in the photo"""
-    results = model.predict(image)
-
-    # Iterations over tensors in order to locate the necessary objects
-    for box in results[0].boxes:
-        if int(box.cls) == 1:
-
-            return True
-
-    return False
